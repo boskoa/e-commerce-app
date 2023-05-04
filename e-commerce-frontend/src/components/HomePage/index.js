bd45d6188460334f4958c8836224478f5cb4d555
boskoa <bosko.ap@gmail.com>
2023-05-04 19:09:02 +0200

    Fixing layout hight

--- e-commerce-frontend/src/components/HomePage/index.js
+++ e-commerce-frontend/src/components/HomePage/index.js
@@ -4,7 +4,8 @@ import PopularProducts from "../../features/popular/PopularProducts";
 import Slider from "../../features/products/Slider";
 
 const Container = styled.div`
-  width: "100%";
+  width: 100%;
+  min-height: 70vh;
 `;
 
 function HomePage() {
